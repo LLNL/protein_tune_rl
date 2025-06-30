07e5cbf71203807c24ffab34449d541a47d42ba6
Mikel Landajuela Larma <landajuelala1@llnl.gov>
2025-06-30 16:46:12 -0700

    Max the batch size

--- protein_tune_rl/dataloader/__init__.py
+++ protein_tune_rl/dataloader/__init__.py
@@ -1,9 +1,17 @@
 import torch.distributed as dist
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
+from protein_tune_rl import logger
 
 
 def create_dataloader(dataset, batch_size, shuffle=True, collate_fn=None):
+
+    if batch_size > len(dataset):
+        logger.warning(
+            f"Batch size {batch_size} is larger than dataset size {len(dataset)}. Adjusting batch size to dataset size."
+        )
+        batch_size = len(dataset)
+
     sampler = DistributedSampler(
         dataset,
         num_replicas=dist.get_world_size(),
